b9fb59560bea876cf59de55ef6d93b7e1315ac24
tbikfalvi <tbikfalvi@gmail.com>
2013-06-14 16:40:10 +0200

    ...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,53 @@
+//============================================================================================================
+//
+//  VTI SAT Installer application
+//
+//  File            : mainwindow.cpp
+//  File version    : 1.0
+//
+//============================================================================================================
+
+#include <QDir>
+#include <QFileDialog>
+#include <QMessageBox>
+
+//============================================================================================================
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+//============================================================================================================
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_qsDirRelease = QDir::currentPath();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_action_Exit_application_triggered()
+{
+    close();
+}
+
+void MainWindow::on_action_Preferences_triggered()
+{
+}
+
+void MainWindow::on_action_Install_Folder_triggered()
+{
+    QString qsDirectory = QFileDialog::getExistingDirectory(this, tr("Select the desired directory"), m_qsDirRelease );
+
+    if (!qsDirectory.isEmpty())
+    {
+        m_qsDirRelease = qsDirectory;
+}
+
+void MainWindow::on_action_Install_triggered()
+{
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_action_Exit_application_triggered();
+    void on_action_Preferences_triggered();
+    void on_action_Install_Folder_triggered();
+    void on_action_Install_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QString         m_qsDirRelease;
 };
 
 #endif // MAINWINDOW_H
